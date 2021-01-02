759d53030c0fd641b966946ad3068b8468938a14
ZalmanKelber <62047666+ZalmanKelber@users.noreply.github.com>
2021-01-01 19:35:44 -0500

    redesigned select type page

--- client/src/components/SelectType.js
+++ client/src/components/SelectType.js
@@ -35,15 +35,58 @@ class SelectType extends React.Component {
                     <div className="type-title solo-melody-title">Solo Melodies:</div>
                     <div className="type-title species-counterpoint-title">Species Counterpoint:</div>
                     <div className="type-title two-part-counterpoint-title">Two Part Counterpoint:</div>
-                    <div className="type-selection cantus-firmus" onClick={() => this.handleClick("cantusFirmus")}>Cantus Firmus</div>
-                    <div className="type-selection free-melody" onClick={() => this.handleClick("freeMelody")}>Free Melody</div>
-                    <div className="type-selection first-species" onClick={() => this.handleClick("twoPartFirstSpecies")}>First Species</div>
-                    <div className="type-selection second-species" onClick={() => this.handleClick("twoPartSecondSpecies")}>Second Species</div>
-                    <div className="type-selection third-species" onClick={() => this.handleClick("twoPartThirdSpecies")}>Third Species</div>
-                    <div className="type-selection fourth-species" onClick={() => this.handleClick("twoPartFourthSpecies")}>Fourth Species</div>
-                    <div className="type-selection fifth-species" onClick={() => this.handleClick("twoPartFifthSpecies")}>Fifth Species</div>
-                    <div className="type-selection free-counterpoint" onClick={() => this.handleClick("twoPartFreeCounterpoint")}>Free Counterpoint</div>
-                    <div className="type-selection imitative-counterpoint" onClick={() => this.handleClick("twoPartImitativeCounterpoint")}>Imitative Counterpoint</div>
+                    <div className="type-selection cantus-firmus" onClick={() => this.handleClick("cantusFirmus")}>
+                        <div className="type-name">Cantus Firmus</div>
+                        <div className="type-description">Simple whole-note melody that exemplifies the most basic rules of 
+                        melody in <a href="https://en.wikipedia.org/wiki/Giovanni_Pierluigi_da_Palestrina" onClick={e => e.stopPropagation()} target="_blank">
+                            Palestrina-style
+                            </a>
+                        </div>
+                    </div>
+                    <div className="type-selection free-melody" onClick={() => this.handleClick("freeMelody")}>
+                        <div className="type-name">Free Melody</div>
+                        <div className="type-description">Complete melody with different rhythmic values; displays all the rules of 
+                        melody in Palestrina-style
+                        </div>
+                    </div>
+                    <div className="type-selection first-species" onClick={() => this.handleClick("twoPartFirstSpecies")}>
+                        <div className="type-name">First Species</div>
+                        <div className="type-description">Whole-note melody set against a Cantus Firmus; exemplifies the most basic rules of harmony:
+                        all intervals between two simultaneous notes are <a href="https://en.wikipedia.org/wiki/Consonance_and_dissonance" onClick={e => e.stopPropagation()} target="_blank">
+                            consonant
+                            </a>
+                        </div>
+                    </div>
+                    <div className="type-selection second-species" onClick={() => this.handleClick("twoPartSecondSpecies")}>
+                        <div className="type-name">Second Species</div>
+                        <div className="type-description">Half-note melody set against a Cantus Firmus; exemplifies simplest rules 
+                        of <a href="https://en.wikipedia.org/wiki/Consonance_and_dissonance" onClick={e => e.stopPropagation()} target="_blank">
+                            dissonance
+                            </a> in harmony: dissonances only occur on weak beats
+                        </div>
+                    </div>
+                    <div className="type-selection third-species" onClick={() => this.handleClick("twoPartThirdSpecies")}>
+                        <div className="type-name">Third Species</div>
+                        <div className="type-description">Quarter-note melody set against a Cantus Firmus; exemplifies more complex rules of dissonance in harmony:
+                        several types of dissonance occur on weak beats</div>
+                    </div>
+                    <div className="type-selection fourth-species" onClick={() => this.handleClick("twoPartFourthSpecies")}>
+                        <div className="type-name">Fourth Species</div>
+                        <div className="type-description">Half-note and syncopated whole-note melody set against a Cantus Firmus; 
+                        dissonances can occur on strong beats in the form of suspensions</div>
+                    </div>
+                    <div className="type-selection fifth-species" onClick={() => this.handleClick("twoPartFifthSpecies")}>
+                        <div className="type-name">Fifth Species</div>
+                        <div className="type-description">Free melody set against a Cantus Firmus; combines all rules of dissonance and harmony</div>
+                    </div>
+                    <div className="type-selection free-counterpoint" onClick={() => this.handleClick("twoPartFreeCounterpoint")}>
+                        <div className="type-name">Free Counterpoint</div>
+                        <div className="type-description">Two free melodies set against each other</div>
+                    </div>
+                    <div className="type-selection imitative-counterpoint" onClick={() => this.handleClick("twoPartImitativeCounterpoint")}>
+                        <div className="type-name">Imitative Counterpoint</div>
+                        <div className="type-description">Two free melodies that begin with the same theme or motif</div>
+                    </div>
                     <div className="preview-image cantus-firmus-preview"></div>
                     <div className="preview-image free-melody-preview"></div>
                     <div className="preview-image first-species-preview"></div>
